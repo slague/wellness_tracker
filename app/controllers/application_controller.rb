f6520572bb10267a305aa4c7b67a8144d0722b42
caward12 <ward.colleen.a@gmail.com>
2017-04-06 15:45:03 -0600

    week features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_week
+
   def current_week
-    Week.last
+    today = Date.today
+    Week.where('start_date < ? AND end_date > ?', today, today)
   end
 end
--- spec/features/admin/weeks/admin_can_create_weeks_spec.rb
+++ spec/features/admin/weeks/admin_can_create_weeks_spec.rb
@@ -13,38 +13,38 @@ RSpec.feature "Admin can create weeks" do
       visit new_admin_week_path
 
       within(".week_0") do
-        fill_in "weeks[][start_date]", with: "4/2/2017"
-        fill_in "weeks[][end_date]", with: "4/8/2017"
+        fill_in "weeks[][start_date]", with: "2/4/2017"
+        fill_in "weeks[][end_date]", with: "8/4/2017"
         select "1", :from =>"weeks[][week_number_id]"
       end
 
       within(".week_1") do
-        fill_in "weeks[][start_date]", with: "4/9/2017"
-        fill_in "weeks[][end_date]", with: "4/15/2017"
+        fill_in "weeks[][start_date]", with: "9/4/2017"
+        fill_in "weeks[][end_date]", with: "15/4/2017"
         select "2", :from =>"weeks[][week_number_id]"
       end
 
       within(".week_2") do
-        fill_in "weeks[][start_date]", with: "4/16/2017"
-        fill_in "weeks[][end_date]", with: "4/22/2017"
+        fill_in "weeks[][start_date]", with: "16/4/2017"
+        fill_in "weeks[][end_date]", with: "22/4/2017"
         select "3", :from =>"weeks[][week_number_id]"
       end
 
       within(".week_3") do
-        fill_in "weeks[][start_date]", with: "4/23/2017"
-        fill_in "weeks[][end_date]", with: "4/29/2017"
+        fill_in "weeks[][start_date]", with: "23/4/2017"
+        fill_in "weeks[][end_date]", with: "29/4/2017"
         select "4", :from =>"weeks[][week_number_id]"
       end
 
       within(".week_4") do
-        fill_in "weeks[][start_date]", with: "4/30/2017"
-        fill_in "weeks[][end_date]", with: "5/6/2017"
+        fill_in "weeks[][start_date]", with: "30/4/2017"
+        fill_in "weeks[][end_date]", with: "6/5/2017"
         select "5", :from =>"weeks[][week_number_id]"
       end
 
       within(".week_5") do
-        fill_in "weeks[][start_date]", with: "5/7/2017"
-        fill_in "weeks[][end_date]", with: "5/13/2017"
+        fill_in "weeks[][start_date]", with: "7/5/2017"
+        fill_in "weeks[][end_date]", with: "13/5/2017"
         select "6", :from =>"weeks[][week_number_id]"
       end
 
@@ -52,12 +52,12 @@ RSpec.feature "Admin can create weeks" do
 
       expect(current_path).to eq(admin_weeks_path)
 
-      expect(page).to have_content("Week 1: 2017-02-04 - 2017-08-04")
-      # expect(page).to have_content("Week 2: 2017-09-04 - 2017-15-04")
-      # expect(page).to have_content("Week 3: 2017-16-04 - 2017-22-04")
-      # expect(page).to have_content("Week 4: 2017-23-04 - 2017-29-04")
-      # expect(page).to have_content("Week 5: 2017-30-04 - 2017-06-05")
-      # expect(page).to have_content("Week 6: 2017-07-05 - 2017-13-05")
+      expect(page).to have_content("Week 1: 2017-04-02 - 2017-04-08")
+      expect(page).to have_content("Week 2: 2017-04-09 - 2017-04-15")
+      expect(page).to have_content("Week 3: 2017-04-16 - 2017-04-22")
+      expect(page).to have_content("Week 4: 2017-04-23 - 2017-04-29")
+      expect(page).to have_content("Week 5: 2017-04-30 - 2017-05-06")
+      expect(page).to have_content("Week 6: 2017-05-07 - 2017-05-13")
       expect(Week.count).to eq(6)
     end
   end
