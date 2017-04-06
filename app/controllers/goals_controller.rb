1008e104978e7f3872430867bc37becf04073483
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-06 01:28:16 -0600

    write funtionality to pass all edit tests

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,76 +1,90 @@
 class GoalsController < ApplicationController
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
-  # GET /goals
-  # GET /goals.json
   def index
     @user = User.find(params[:user_id])
     @goals = @user.goals
   end
 
-  # GET /goals/1
-  # GET /goals/1.json
   def show
-    # @user = User.find(params[:user_id])
-    # @goal = @user.goals.find(params[:id])
   end
 
-  # GET /goals/new
   def new
     @user = User.find(params[:user_id])
     @goal = Goal.new
   end
 
-  # GET /goals/1/edit
   def edit
     @user = User.find(params[:user_id])
     @goal = @user.goals.find(params[:id])
   end
 
-  # POST /goals
-  # POST /goals.json
   def create
     @user = User.find(params[:user_id])
-    @goal = @user.goals.new(goal_params.merge(week: current_week))
+    @goal = @user.goals.new(goal_params)
+    new_total = goal_params[:total_goal_count]
 
-    if @goal.save
+    if @goal.reasonable_total?(new_total) && @goal.save
       flash[:success] = "Goal was successfully created."
       redirect_to user_goals_path(@user)
+    elsif
+      flash[:danger] = "#{@goal.category.name} category's goal sum cannot exceed seven!"
+      render :new
     else
       render :new
     end
   end
 
-  # PATCH/PUT /goals/1
-  # PATCH/PUT /goals/1.json
   def update
-
     @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
+    new_total = goal_params[:total_goal_count]
 
-    if @user.goals.update(goal_params.merge(week: current_week))
+    new_progress += 1 if params[:goal].nil?
+
+    if @goal.reasonable_total?(new_total) && @goal.update(goal_params)
       flash[:success] = 'Goal was successfully updated.'
       redirect_to user_goals_path(@user)
+    elsif
+      flash[:danger] = "#{@goal.category.name} category's goal sum cannot exceed seven!"
+      render :edit
     else
       render :edit
     end
   end
 
-  # DELETE /goals/1
-  # DELETE /goals/1.json
+  def increment
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
+    new_count = @goal.progress_count + 1
+
+    if @goal.update(progress_count: new_count) && (@goal.total_goal_count == new_count)
+      flash[:success] = 'You achieved your goal for the week! Awesome job.'
+      redirect_to user_goals_path(@user)
+    elsif @goal.update(progress_count: new_count)
+      flash[:success] = 'Nicely done!'
+      redirect_to user_goals_path(@user)
+    else
+      flash[:danger] = 'Unable to increment goal.'
+      redirect_to user_goals_path(@user)
+    end
+
+  end
+
   def destroy
     @goal.destroy
     flash[:success] = "Goal Successfully Deleted!"
-      redirect_to user_goals_path
+    redirect_to user_goals_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_goal
       @goal = Goal.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def goal_params
-      params.require(:goal).permit(:description, :progress_count, :total_goal_count, :category_id, :user_id, :week_id)
+      params.require(:goal).permit(:description,
+                                   :total_goal_count,
+                                   :category_id).merge(week: current_week)
     end
 end
--- spec/features/goals/user_can_edit_an_existing_goal_spec.rb
+++ spec/features/goals/user_can_edit_an_existing_goal_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "User can edit a goal" do
       expect(current_path).to eq(edit_user_goal_path(@goal.user, @goal))
       expect(page).to_not have_content("Category")
     end
-    xscenario "user can update description" do
+    scenario "user can update description" do
       visit edit_user_goal_path(@goal.user, @goal)
 
       fill_in "goal[description]", with: "Be nicer to Lauren"
@@ -24,7 +24,7 @@ RSpec.feature "User can edit a goal" do
       expect(page).to have_content('Goal was successfully updated.')
       expect(page).to have_content("Be nicer to Lauren")
     end
-    xscenario "user can update total goal count" do
+    scenario "user can update total goal count" do
       visit edit_user_goal_path(@goal.user, @goal)
 
       fill_in "goal[total_goal_count]", with: 6
@@ -32,11 +32,11 @@ RSpec.feature "User can edit a goal" do
       click_on "Update Goal"
 
       expect(current_path).to eq(user_goals_path(@goal.user))
-      expect(@goal.total_goal_count).to eq(6)
       expect(page).to have_content('Goal was successfully updated.')
       expect(page).to have_content("6")
+      expect(Goal.find(@goal.id).total_goal_count).to eq(6)
     end
-    xscenario "user cannot update total goal count to be more than seven" do
+    scenario "user cannot update total goal count to be more than seven" do
       original_goal_count = @goal.total_goal_count
 
       visit edit_user_goal_path(@goal.user, @goal)
@@ -45,43 +45,55 @@ RSpec.feature "User can edit a goal" do
 
       click_on "Update Goal"
 
-      expect(page).to have_content('Goal cannot be more than seven.')
-      expect(@goal.total_goal_count).to eq(original_goal_count)
+      expect(page).to have_content("#{@goal.category.name} category's goal sum cannot exceed seven!")
+      expect(Goal.find(@goal.id).total_goal_count).to eq(original_goal_count)
     end
-    xscenario "user cannot update total goal count to be more than seven for a category" do
+    scenario "user cannot update total goal count to be more than seven for a category" do
       user = create(:user_with_goals)
       category = user.goals[0].category
       user.goals[0].update(total_goal_count: 1)
       user.goals[1].update(total_goal_count: 1, category: category)
       user.goals[2].update(total_goal_count: 1, category: category)
 
-      expect(user.goal_total(category)).to eq(3)
+      expect(user.goals[2].category_total).to eq(3)
 
       goal1, goal2, goal3 = user.goals
-
       original_goal_count = goal1.total_goal_count
 
       visit edit_user_goal_path(user, goal1)
 
-      fill_in "goal[total_goal_count]", with: 5
+      fill_in "goal[total_goal_count]", with: 6
 
       click_on "Update Goal"
 
-      expect(page).to have_content('You cannot have more than seven goals for #{category.name}.')
-      expect(goal1.total_goal_count).to eq(original_goal_count)
+      expect(page).to have_content("#{goal1.category.name} category's goal sum cannot exceed seven!")
+      expect(Goal.find(goal1.id).total_goal_count).to eq(original_goal_count)
     end
-    xscenario "user can update progress count with button" do
+    scenario "user can update progress count with button" do
       original_goal_count = @goal.progress_count
 
       visit user_goals_path(@goal.user)
 
       click_button "Increment Goal Progress!"
 
-      expect(page).to have_content('Good job!')
+      expect(page).to have_content('Nicely done!')
       expect(page).to have_content(original_goal_count + 1)
     end
-    xscenario "user cannot update progress count if goal has been met" do
-      @goal.update(progress_count: total_goal_count)
+    scenario "user receives achievement messages when goal is met" do
+      original_goal_count = @goal.progress_count
+      num = @goal.total_goal_count - original_goal_count
+
+      visit user_goals_path(@goal.user)
+
+      num.times do
+        click_button "Increment Goal Progress!"
+      end
+
+      expect(page).to have_content("You achieved your goal for the week! Awesome job.")
+      expect(Goal.find(@goal.id).progress_count).to eq(@goal.total_goal_count)
+    end
+    scenario "user cannot update progress count if goal has been met" do
+      @goal.update(progress_count: @goal.total_goal_count)
 
       visit user_goals_path(@goal.user)
 
