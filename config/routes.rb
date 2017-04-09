f9fb9f03e3f03041694f798af9edd09b6449b8f6
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-09 11:28:00 -0600

    change provider back

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root                                 to: 'sessions#show'
-  get     '/auth/census/callback',     to: 'sessions#create'
+  get     '/auth/:provider/callback',  to: 'sessions#create'
   delete '/logout',                    to: 'sessions#destroy'
   get   '/sign_in_with_census',        to: 'sessions#new'
 
