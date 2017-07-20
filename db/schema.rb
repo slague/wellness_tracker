dc87e8266caa9043804eb61d71f38b0d5e759340
slague <stephanielague@gmail.com>
2017-07-20 09:44:55 -0600

    Add cohort to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719195303) do
+ActiveRecord::Schema.define(version: 20170720152024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170719195303) do
     t.datetime "updated_at",          null: false
     t.integer  "role"
     t.string   "census_access_token"
+    t.string   "cohort"
   end
 
   create_table "week_numbers", force: :cascade do |t|
