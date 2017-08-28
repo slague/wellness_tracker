aaf09a1ebd9b09907c33b23038eb3c7a77d91a44
slague <stephanielague@gmail.com>
2017-08-28 10:39:38 -0600

    Add config mountain time to application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,7 +25,8 @@ module WellnessTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.time_zone = 'Mountain Time (US & Canada)'
+    config.active_record.default_timezone = :local
     # configure scaffolding
     config.generators do |g|
       g.orm             :active_record
